9cabb2b20ad04a6aa436e4f24953ccc3a51e5254
MaulikGupta27 <guptamaulik27@gmail.com>
2024-07-19 18:38:32 +0530

    added day1.js file

---
+++ day1.js
@@ -0,0 +1,55 @@
+var a = 7;
+console.log(a);
+
+let b = "maulik";
+console.log(b);
+
+const c = true;
+console.log(c);
+
+let d = 28;
+let e = "helloworld";
+let f = false;
+let g = { name: "Maulik", age: 19 };
+let h = [1, 2, 3, 4, 5];
+
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+console.log(typeof h);
+
+
+let i = 30;
+console.log(i);
+i = 40;
+console.log(i);
+
+//showing error thats why commented
+// const j = 50;
+// console.log(j);
+// j = 60;
+// console.log(j);
+
+
+let number = 754;
+let string = "This is a string";
+let boolean = true;
+let object = { key: "value" };
+let array = [1, 2, 3, 4, 5];
+
+console.log("Value: ", number, ", Type: ", typeof number);
+console.log("Value: ", string, ", Type: ", typeof string);
+console.log("Value: ", boolean, ", Type: ", typeof boolean);
+console.log("Value: ", object, ", Type: ", typeof object);
+console.log("Value: ", array, ", Type: ", typeof array);
+
+let variable = "old";
+console.log(variable);
+variable = "new";
+console.log(variable);
+
+const newVar = "old";
+console.log(newVar);
+// newVar = "new";
+// console.log(newVar);
