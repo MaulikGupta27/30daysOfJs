d553eb93e2ce50c33b1003c7df287f651a596b9f
MaulikGupta27 <guptamaulik27@gmail.com>
2024-07-20 10:04:33 +0530

    added day2.js file

---
+++ day2.js
@@ -0,0 +1,54 @@
+//Activity -1
+let a = 10, b = 2;
+
+// addition operator
+console.log("Addition =", a + b);
+// subtraction operator
+console.log("Subtraction =", a - b);
+// multiplication operator
+console.log("Multiplication =", a * b);
+// division operator
+console.log("Division =", a / b);
+// remainder operator
+console.log("Remainder =", a % b);
+
+
+//Activity -2
+let c = 5;
+
+// addition assignment
+c += 5;
+console.log("Addition =", c);
+// subtraction assignment
+c - +5;
+console.log("Subtraction =", c);
+
+
+//Activity -3
+
+//Using < and >
+console.log("10>20 =", 10 > 20);
+console.log("10<20 =", 10 < 20);
+//Using <= and >=
+console.log("10>=20 =", 10 >= 20);
+console.log("10<=20 =", 10 <= 20);
+//Using == and ===
+console.log("10==20 =", 10 == 20);
+console.log("10===20 =", 10 === 20);
+
+
+//Activity -4
+let x = 4;
+
+//Using && operator
+console.log("(x>3)&&(x<2) =", (x > 3) && (x < 2));
+//Using !! operator
+console.log("(x>3)||(x<2) =", (x > 3) || (x < 2));
+//Using ! operator
+console.log("!(x==4) =", !(x == 4));
+
+//Activity -5
+
+let num = -12;
+let check = (num > 0) ? 'Positive' : 'Negative';
+console.log(check);
