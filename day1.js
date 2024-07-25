f3ad702a97824393a0a34c64381874bdcb508515
MaulikGupta27 <guptamaulik27@gmail.com>
2024-07-25 08:35:50 +0530

    updated file

--- day1.js
+++ day1.js
@@ -1,37 +1,56 @@
+//Activity -1
+
+//Task 1
 var a = 7;
-console.log(a);
+console.log(a); //output: 7
 
+//Task 2
 let b = "maulik";
-console.log(b);
+console.log(b); //output: maulik
 
+
+//Activity -2
+
+//Task 3
 const c = true;
-console.log(c);
+console.log(c); //output: true
 
+
+//Activity -3
+
+//Task 4
 let d = 28;
 let e = "helloworld";
 let f = false;
 let g = { name: "Maulik", age: 19 };
 let h = [1, 2, 3, 4, 5];
+                        //output:
+console.log(typeof d);      //number
+console.log(typeof e);      //string
+console.log(typeof f);      //boolean
+console.log(typeof g);      //object
+console.log(typeof h);      //object
 
-console.log(typeof d);
-console.log(typeof e);
-console.log(typeof f);
-console.log(typeof g);
-console.log(typeof h);
 
+//Activity -4
 
+//Task 5
 let i = 30;
-console.log(i);
+console.log(i);  //output: 30
 i = 40;
-console.log(i);
+console.log(i);  //output: 40
 
-//showing error thats why commented
-// const j = 50;
-// console.log(j);
+
+//Activity -5
+
+//Task 6
+const j = 50;    //output: 50
+console.log(j);
 // j = 60;
-// console.log(j);
+// console.log(j);   //output: error
 
 
+//Feature request
 let number = 754;
 let string = "This is a string";
 let boolean = true;
@@ -51,5 +70,5 @@ console.log(variable);
 
 const newVar = "old";
 console.log(newVar);
-// newVar = "new";
-// console.log(newVar);
+newVar = "new";
+console.log(newVar);
