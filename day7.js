5632d7ef6b1fc59ef92ca6f0a072a4ad0e03f232
MaulikGupta27 <guptamaulik27@gmail.com>
2024-07-25 12:53:09 +0530

    updated file

---
+++ day7.js
@@ -0,0 +1,18 @@
+//Activity -1: Object creation and access
+
+//Task 1
+const book = {
+    title: "Programming",
+    author: "Maulik",
+    year: 2023,
+}
+console.log(book); //output: { title: 'Programming', author: 'Maulik', year: 2023 }
+
+//Task 2
+console.log(book["title"]); //output: Programming
+console.log(book["author"]);       // Maulik
+
+
+//Activity -2: Object Methods
+
+//Task 3
